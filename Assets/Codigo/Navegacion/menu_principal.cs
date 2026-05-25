2dacb61303bfdff62c988641defa82f8b3cf4180
unknown <hoiimendless@gmail.com>
2026-05-24 23:58:05 -0600

    [EDITAR] detalles y texturas

---
+++ Assets/Codigo/Navegacion/menu_principal.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_principal : MonoBehaviour
+{
+   public void ComenzarJuego(){
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void EmpezarNivel1(){
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void EmpezarNivel2(){
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void EmpezarNivel3(){
+        SceneManager.LoadSceneAsync(3);
+    }
+        
+}
